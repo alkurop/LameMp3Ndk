4250180d1f3b8c0ff9af0d8c374c33dd9b715eda
Alex Kurop <alkurop@gmail.com>
2016-09-07 23:12:02 +0300

    fixed permission problem

--- app/src/main/java/com/omar/retromp3recorder/app/views/MainActivity.java
+++ app/src/main/java/com/omar/retromp3recorder/app/views/MainActivity.java
@@ -1,18 +1,31 @@
 package com.omar.retromp3recorder.app.views;
 
+import android.Manifest;
+import android.content.Intent;
 import android.media.audiofx.Visualizer;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
+
+import com.github.alkurop.permissionmanager.PermissionRequiredDetails;
+import com.github.alkurop.permissionmanager.PermissionsManager;
 import com.omar.retromp3recorder.app.R;
 import com.omar.retromp3recorder.app.customviews.VisualizerView;
 import com.omar.retromp3recorder.app.presenters.IMainEvents;
 import com.omar.retromp3recorder.app.presenters.MainPresenter;
 import com.omar.retromp3recorder.app.utils.TouchController;
+import com.squareup.picasso.Picasso;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import kotlin.Unit;
+import kotlin.jvm.functions.Function1;
 
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener, IMainView {
@@ -24,6 +37,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private TextView tv_Timer;
     private VisualizerView mVisualizerView;
     private Visualizer mVisualizer;
+    private PermissionsManager permissionManager;
+    private ImageView background;
 
 
     @Override
@@ -31,9 +46,48 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         presenter = new MainPresenter();
         presenter.init(this);
+        permissionManager = new PermissionsManager(this);
+        HashMap<String, PermissionRequiredDetails> permissions = new HashMap<>();
+        permissions.put(Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                new PermissionRequiredDetails(getString(R.string.write_permission_title),
+                        getString(R.string.write_permission_message),
+                        getString(R.string.write_required_message)));
+        permissions.put(Manifest.permission.RECORD_AUDIO,
+                new PermissionRequiredDetails(getString(R.string.write_permission_title),
+                        getString(R.string.write_permission_message),
+                        getString(R.string.write_required_message)));
+        permissionManager.addPermissions(permissions);
     }
 
+    @Override
+    protected void onStart () {
+        super.onStart();
+        permissionManager.addPermissionsListener(new Function1<Map<String, Boolean>, Unit>() {
+            @Override
+            public Unit invoke (Map<String, Boolean> stringBooleanMap) {
+                permissionManager.clearPermissionsListeners();
+                for(Map.Entry<String, Boolean> entry: stringBooleanMap.entrySet()){
+                    if (!entry.getValue()) {
+                        return null;
+                    }
+                }
+                return null;
+            }
+        });
+        permissionManager.makePermissionRequest(true);
+    }
 
+    @Override
+    public void onRequestPermissionsResult (int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        permissionManager.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    @Override
+    protected void onActivityResult (int requestCode, int resultCode, Intent data) {
+        permissionManager.onActivityResult(requestCode);
+        super.onActivityResult(requestCode, resultCode, data);
+    }
 
     @Override
     public void onClick(View view) {
@@ -48,9 +102,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.iv_share:
                 presenter.shareClicked();
                 break;
-
         }
-
     }
 
     @Override
@@ -64,7 +116,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         mVisualizerView = (VisualizerView) findViewById(R.id.visualizer);
         llRadioContainer1 = (LinearLayout) findViewById(R.id.ll_radio_container1);
         llRadioContainer2 = (LinearLayout) findViewById(R.id.ll_radio_container2);
-
+        background = ((ImageView) findViewById(R.id.background));
+        Picasso.with(this).load(R.drawable.bg).fit().into(background);
 
         btn_Play.setOnClickListener(this);
         btn_Record.setOnClickListener(this);
