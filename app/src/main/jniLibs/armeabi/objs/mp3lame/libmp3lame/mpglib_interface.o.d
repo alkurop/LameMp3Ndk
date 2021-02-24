3c4934fb3811ef2c4b262b0cfdec03899f890cd8
Alexey Kuropiatnyk <alexey.kuropiantnyk@ah.nl>
2021-02-24 21:12:45 +0100

    Properly configure jni build

--- app/src/main/jniLibs/armeabi/objs/mp3lame/libmp3lame/mpglib_interface.o.d
+++
@@ -1,2 +0,0 @@
-/media/omar/d/AndroidStudioProjects/LameMp3Ndk/app/src/main/obj/local/armeabi/objs/mp3lame/libmp3lame/mpglib_interface.o: \
- /media/omar/d/AndroidStudioProjects/LameMp3Ndk/app/src/main/jni/libmp3lame/mpglib_interface.c
--- app/src/main/jniLibs/x86/objs/mp3lame/libmp3lame/mpglib_interface.o.d
+++
@@ -1,2 +0,0 @@
-/media/omar/d/AndroidStudioProjects/LameMp3Ndk/app/src/main/obj/local/x86/objs/mp3lame/libmp3lame/mpglib_interface.o: \
- /media/omar/d/AndroidStudioProjects/LameMp3Ndk/app/src/main/jni/libmp3lame/mpglib_interface.c
