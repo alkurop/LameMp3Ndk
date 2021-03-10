ed64e9bb555a67cc90588584f6e8f2d180e52642
Alexey Kuropiatnyk <alexey.kuropiantnyk@ah.nl>
2021-03-10 01:30:44 +0100

    Remove files explorer

--- app/src/main/java/com/omar/retromp3recorder/app/di/AppComponent.kt
+++ app/src/main/java/com/omar/retromp3recorder/app/di/AppComponent.kt
@@ -1,6 +1,5 @@
 package com.omar.retromp3recorder.app.di
 
-import com.omar.retromp3recorder.app.files.FilesFragment
 import com.omar.retromp3recorder.app.main.MainActivity
 import dagger.Component
 import javax.inject.Singleton
@@ -8,7 +7,6 @@ import javax.inject.Singleton
 @Singleton
 @Component(modules = [ConfigModule::class, FunctionalityModule::class, UtilsModule::class, UseCaseModule::class])
 interface AppComponent {
-    fun inject(filesFragment: FilesFragment)
     fun inject(mainActivity: MainActivity)
 
     companion object {
