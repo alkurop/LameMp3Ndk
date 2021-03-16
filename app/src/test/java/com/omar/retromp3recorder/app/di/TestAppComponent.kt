c6b3719628b6ab3d6aae26816a7f64a0a81eaa7f
Alexey Kuropiatnyk <alexey.kuropiantnyk@ah.nl>
2021-03-16 21:34:47 +0100

    Fix failed tests

--- app/src/test/java/com/omar/retromp3recorder/app/di/TestAppComponent.kt
+++ app/src/test/java/com/omar/retromp3recorder/app/di/TestAppComponent.kt
@@ -2,9 +2,6 @@ package com.omar.retromp3recorder.app.di
 
 import com.omar.retromp3recorder.app.ui.main.MainViewInteractorActionsTest
 import com.omar.retromp3recorder.app.ui.main.MainViewInteractorRepoTest
-import com.omar.retromp3recorder.bl.ChangeBitrateUCTest
-import com.omar.retromp3recorder.bl.ChangeSampleRateUCImplTest
-import com.omar.retromp3recorder.bl.ShareUCImplTest
 import dagger.Component
 import javax.inject.Singleton
 
@@ -12,8 +9,6 @@ import javax.inject.Singleton
 @Component(modules = [ConfigModule::class, TestFunctionalityModule::class, TestUtilsModule::class, MockModule::class])
 interface TestAppComponent {
     fun inject(mainViewInteractorRepoTest: MainViewInteractorRepoTest)
+
     fun inject(mainViewInteractorActionsTest: MainViewInteractorActionsTest)
-    fun inject(changeBitrateUCImplTest: ChangeBitrateUCTest)
-    fun inject(changeSampleRateUCImplTest: ChangeSampleRateUCImplTest)
-    fun inject(shareUCImplTest: ShareUCImplTest)
 }
