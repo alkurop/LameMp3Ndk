85c48acf26f071cf3974fc35b253a98a8435a185
Alexey Kuropiatnyk <alexey.kuropiantnyk@ah.nl>
2021-04-06 18:49:06 +0200

    Introduce three lines repo

---
+++ repos/src/main/java/com/omar/retromp3recorder/state/repos/BehaviorSubjectRepo.kt
@@ -0,0 +1,16 @@
+package com.omar.retromp3recorder.state.repos
+
+import io.reactivex.Observable
+import io.reactivex.subjects.BehaviorSubject
+
+open class BehaviorSubjectRepo<T : Any>(default: T? = null) {
+    private val behaviorSubject: BehaviorSubject<T> =
+        if (default == null) BehaviorSubject.create()
+        else BehaviorSubject.createDefault(default)
+
+    fun onNext(next: T) {
+        behaviorSubject.onNext(next)
+    }
+
+    fun observe(): Observable<T> = behaviorSubject
+}
--- repos/src/main/java/com/omar/retromp3recorder/state/repos/RequestPermissionsRepo.kt
+++ repos/src/main/java/com/omar/retromp3recorder/state/repos/RequestPermissionsRepo.kt
@@ -1,22 +1,11 @@
 package com.omar.retromp3recorder.state.repos
 
 import com.github.alkurop.ghostinshell.Shell
-import io.reactivex.Observable
-import io.reactivex.subjects.BehaviorSubject
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
-class RequestPermissionsRepo @Inject constructor() {
-    private val state: BehaviorSubject<ShouldRequestPermissions> = BehaviorSubject.create()
-    fun observe(): Observable<ShouldRequestPermissions> {
-        return state
-    }
-
-    fun onNext(newValue: ShouldRequestPermissions) {
-        state.onNext(newValue)
-    }
-
+class RequestPermissionsRepo @Inject constructor() : BehaviorSubjectRepo<RequestPermissionsRepo.ShouldRequestPermissions>(){
     sealed class ShouldRequestPermissions {
         object Granted : ShouldRequestPermissions()
         data class Denied(val permissions: Shell<Set<String>>) : ShouldRequestPermissions()
