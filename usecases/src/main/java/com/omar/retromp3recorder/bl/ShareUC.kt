e086af5204acf76146e7af5357c087c499f55338
Alex K <alkurop@gmail.com>
2021-04-08 10:13:20 +0200

    Fix broken share uc test

--- usecases/src/main/java/com/omar/retromp3recorder/bl/ShareUC.kt
+++ usecases/src/main/java/com/omar/retromp3recorder/bl/ShareUC.kt
@@ -1,12 +1,13 @@
 package com.omar.retromp3recorder.bl
 
+import com.omar.retromp3recorder.share.Sharer
 import com.omar.retromp3recorder.state.repos.CurrentFileRepo
 import io.reactivex.Completable
 import java.io.File
 import javax.inject.Inject
 
 class ShareUC @Inject constructor(
-    private val sharingModule: com.omar.retromp3recorder.share.Sharer,
+    private val sharingModule: Sharer,
     private val currentFileRepo: CurrentFileRepo
 ) {
     fun execute(): Completable {
