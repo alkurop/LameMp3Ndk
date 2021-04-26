3d7f86fd861a7b48852c2525cece18534c994b2d
Alexey Kuropiatnyk <alexey.kuropiantnyk@ah.nl>
2021-04-26 16:07:09 +0200

    DeleteFileOutputMapperTest.kt

---
+++ app/src/test/java/com/omar/retromp3recorder/app/ui/files/edit/delete/DeleteFileOutputMapperTest.kt
@@ -0,0 +1,29 @@
+package com.omar.retromp3recorder.app.ui.files.edit.delete
+
+import io.reactivex.observers.TestObserver
+import io.reactivex.subjects.PublishSubject
+import io.reactivex.subjects.Subject
+import org.junit.Before
+import org.junit.Test
+
+class DeleteFileOutputMapperTest {
+    private lateinit var test: TestObserver<DeleteFileView.State>
+    private val outputPublisher: Subject<DeleteFileView.Output> = PublishSubject.create()
+
+    @Before
+    fun setUp() {
+        test = outputPublisher.compose(DeleteFileOutputMapper.mapOutputToState()).test()
+    }
+
+    @Test
+    fun `starts with dismiss false`() {
+        test.assertValue(DeleteFileView.State(shouldDismiss = false))
+    }
+
+    @Test
+    fun `on finished output dismiss true`() {
+        outputPublisher.onNext(DeleteFileView.Output.Finished)
+
+        test.assertValueAt(1, DeleteFileView.State(shouldDismiss = true))
+    }
+}
