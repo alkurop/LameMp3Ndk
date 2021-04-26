e317fea629fb7a2f7e26c6423a200efd15eb1898
Alexey Kuropiatnyk <alexey.kuropiantnyk@ah.nl>
2021-04-26 15:21:46 +0200

    DeleteFileInteractorTest.kt

--- app/src/main/java/com/omar/retromp3recorder/app/ui/files/edit/delete/DeleteFileInteractor.kt
+++ app/src/main/java/com/omar/retromp3recorder/app/ui/files/edit/delete/DeleteFileInteractor.kt
@@ -7,16 +7,19 @@ import io.reactivex.Scheduler
 import javax.inject.Inject
 
 class DeleteFileInteractor @Inject constructor(
-        private val deleteFileUC: DeleteFileUC,
-        private val scheduler: Scheduler
+    private val deleteFileUC: DeleteFileUC,
+    private val scheduler: Scheduler
 ) {
-    fun processIO(): ObservableTransformer<DeleteFileView.Input, DeleteFileView.Output> = ObservableTransformer { upstream ->
-        upstream.observeOn(scheduler)
+    fun processIO(): ObservableTransformer<DeleteFileView.Input, DeleteFileView.Output> =
+        ObservableTransformer { upstream ->
+            upstream.observeOn(scheduler)
                 .compose { input ->
                     input
-                            .ofType(DeleteFileView.Input.DeleteFile::class.java)
-                            .flatMapCompletable { deleteFileUC.execute(it.filePath) }
-                            .andThen(Observable.just(DeleteFileView.Output.Finished))
+                        .ofType(DeleteFileView.Input.DeleteFile::class.java)
+                        .flatMap {
+                            deleteFileUC.execute(it.filePath)
+                                .andThen(Observable.just(DeleteFileView.Output.Finished))
+                        }
                 }
-    }
+        }
 }
--- app/src/test/java/com/omar/retromp3recorder/app/di/TestAppComponent.kt
+++ app/src/test/java/com/omar/retromp3recorder/app/di/TestAppComponent.kt
@@ -2,6 +2,7 @@ package com.omar.retromp3recorder.app.di
 
 import com.omar.retromp3recorder.app.ui.audio_controls.AudioControlsInteractorInputTest
 import com.omar.retromp3recorder.app.ui.audio_controls.AudioControlsInteractorOutputTest
+import com.omar.retromp3recorder.app.ui.files.edit.delete.DeleteFileInteractorTest
 import com.omar.retromp3recorder.app.ui.files.edit.selector.SelectorInteractorInputTest
 import com.omar.retromp3recorder.app.ui.files.edit.selector.SelectorInteractorOutputTest
 import com.omar.retromp3recorder.app.ui.files.preview.CurrentFileInteractorInputTest
@@ -40,4 +41,5 @@ interface TestAppComponent {
     fun inject(currentFileOutputMapperTest: CurrentFileInteractorOutputTest)
     fun inject(selectorInteractorInputTest: SelectorInteractorInputTest)
     fun inject(selectorInteractorOutputTest: SelectorInteractorOutputTest)
+    fun inject(deleteFileInteractorTest: DeleteFileInteractorTest)
 }
