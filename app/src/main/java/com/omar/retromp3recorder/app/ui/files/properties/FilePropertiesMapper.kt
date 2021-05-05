64ae6056162f6992d944843828ff9299a5d17914
Alexey Kuropiatnyk <alkurop@gmail.com>
2021-05-05 11:52:37 +0200

    PropertiesFragment.kt basic ui

--- app/src/main/java/com/omar/retromp3recorder/app/ui/files/properties/FilePropertiesMapper.kt
+++ app/src/main/java/com/omar/retromp3recorder/app/ui/files/properties/FilePropertiesMapper.kt
@@ -4,21 +4,29 @@ import com.omar.retromp3recorder.storage.db.AppDatabase
 import com.omar.retromp3recorder.storage.db.toFileWrapper
 import com.omar.retromp3recorder.storage.repo.CurrentFileRepo
 import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.core.Scheduler
 import javax.inject.Inject
 
 class FilePropertiesMapper @Inject constructor(
+    private val appDatabase: AppDatabase,
     private val currentFileRepo: CurrentFileRepo,
-    private val appDatabase: AppDatabase
+    private val scheduler: Scheduler
 ) {
     fun observe(): Observable<PropertiesView.Output.CurrentFileProperties> {
-        return currentFileRepo.observe().switchMap { currentFile ->
-            val filepath = currentFile.value
-            if (filepath == null) Observable.just(PropertiesView.Output.CurrentFileProperties(null))
-            else Observable.fromCallable {
-                PropertiesView.Output.CurrentFileProperties(
-                    appDatabase.fileEntityDao().getByFilepath(filepath)[0].toFileWrapper()
+        return currentFileRepo.observe()
+            .switchMap { currentFile ->
+                val filepath = currentFile.value
+                if (filepath == null) Observable.just(
+                    PropertiesView.Output.CurrentFileProperties(
+                        null
+                    )
                 )
+                else Observable.fromCallable {
+                    PropertiesView.Output.CurrentFileProperties(
+                        appDatabase.fileEntityDao().getByFilepath(filepath)[0].toFileWrapper()
+                    )
+                }
             }
-        }
+            .subscribeOn(scheduler)
     }
 }
--- app/src/main/java/com/omar/retromp3recorder/app/ui/files/properties/PropertiesFragment.kt
+++ app/src/main/java/com/omar/retromp3recorder/app/ui/files/properties/PropertiesFragment.kt
@@ -1,7 +1,31 @@
 package com.omar.retromp3recorder.app.ui.files.properties
 
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.omar.retromp3recorder.app.R
+import com.omar.retromp3recorder.app.ui.utils.fileName
+import com.omar.retromp3recorder.app.ui.utils.findViewById
+import com.omar.retromp3recorder.app.uiutils.observe
 
 class PropertiesFragment : Fragment(R.layout.fragment_properties) {
+    private val viewModel by viewModels<PropertiesViewModel>()
+    private val titleView: TextView
+        get() = findViewById(R.id.title)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel.state.observe(viewLifecycleOwner, ::render)
+    }
+
+    private fun render(state: PropertiesView.State) {
+        val currentFile = state.currentFile
+        requireView().isVisible = currentFile != null
+        if (currentFile != null) {
+            titleView.text = currentFile.path.fileName()
+        }
+    }
 }
