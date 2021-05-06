a49bf2129327a8e7caac9f3c86ebcd3a18ed21c1
Alexey Kuropiatnyk <alexey.kuropiantnyk@ah.nl>
2021-05-06 20:14:32 +0200

    Ditch crashlytics

--- app/src/main/java/com/omar/retromp3recorder/app/App.kt
+++ app/src/main/java/com/omar/retromp3recorder/app/App.kt
@@ -1,7 +1,6 @@
 package com.omar.retromp3recorder.app
 
 import android.app.Application
-import com.google.firebase.crashlytics.FirebaseCrashlytics
 import com.omar.retromp3recorder.app.di.AppComponent
 import com.omar.retromp3recorder.app.di.DaggerAppComponent
 import com.omar.retromp3recorder.app.di.UtilsModule
@@ -18,15 +17,6 @@ class App : Application() {
         appComponent = DaggerAppComponent.builder().utilsModule(UtilsModule(this)).build()
         if (BuildConfig.DEBUG) {
             Timber.plant(Timber.DebugTree())
-        } else {
-            FirebaseCrashlytics.getInstance().setCrashlyticsCollectionEnabled(true)
-            Timber.plant(object : Timber.Tree() {
-                override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
-                    t?.let {
-                        FirebaseCrashlytics.getInstance().recordException(it)
-                    }
-                }
-            })
         }
         appComponent.inject(this)
         loadRecorderSettingsUC.execute().subscribe()
