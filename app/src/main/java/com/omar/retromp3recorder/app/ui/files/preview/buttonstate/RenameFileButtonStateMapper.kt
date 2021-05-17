8737aa75f627b37f1838bf00d7ab610f1c83159b
Alexey Kuropiatnyk <alkurop@gmail.com>
2021-05-17 10:26:34 +0200

    Fix rename fragment open in wrong state crash

---
+++ app/src/main/java/com/omar/retromp3recorder/app/ui/files/preview/buttonstate/RenameFileButtonStateMapper.kt
@@ -0,0 +1,26 @@
+package com.omar.retromp3recorder.app.ui.files.preview.buttonstate
+
+import com.omar.retromp3recorder.bl.audio.AudioState
+import com.omar.retromp3recorder.bl.audio.AudioStateMapper
+import com.omar.retromp3recorder.bl.files.CurrentFileMapper
+import io.reactivex.rxjava3.core.Observable
+import javax.inject.Inject
+
+class RenameFileButtonStateMapper @Inject constructor(
+    private val audioStateMapper: AudioStateMapper,
+    private val currentFileMapper: CurrentFileMapper
+) {
+    fun observe(): Observable<Boolean> {
+        return Observable.combineLatest(
+            currentFileMapper.observe(),
+            audioStateMapper.observe(),
+            { currentFile, audioState ->
+                when (audioState) {
+                    is AudioState.Recording,
+                    is AudioState.Playing -> false
+                    is AudioState.Idle -> currentFile.value != null
+                }
+            }
+        )
+    }
+}
