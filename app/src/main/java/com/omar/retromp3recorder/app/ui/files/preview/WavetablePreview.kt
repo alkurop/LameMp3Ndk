a67a6e6160e8b6d6ffc1893d773e299640a72164
Alexey Kuropiatnyk <alexey.kuropiantnyk@ah.nl>
2021-05-26 13:01:47 +0200

    Clean up wavetable

--- app/src/main/java/com/omar/retromp3recorder/app/ui/files/preview/WavetablePreview.kt
+++ app/src/main/java/com/omar/retromp3recorder/app/ui/files/preview/WavetablePreview.kt
@@ -2,27 +2,21 @@ package com.omar.retromp3recorder.app.ui.files.preview
 
 import android.annotation.SuppressLint
 import android.content.Context
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.Paint
-import android.graphics.Rect
+import android.graphics.*
 import android.util.AttributeSet
 import android.view.View
 import io.reactivex.rxjava3.subjects.BehaviorSubject
+import kotlin.math.max
 
 class WavetablePreview @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : View(context, attrs, defStyleAttr) {
-    private var mPoints: FloatArray = FloatArray(0)
-    private val mRect = Rect()
-    private val mForePaint = Paint()
-    private val bytesBus = BehaviorSubject.create<ByteArray>()
-
-    init {
-        mForePaint.strokeWidth = 1f
-        mForePaint.isAntiAlias = true
-        mForePaint.color = Color.rgb(0, 255, 0)
+    private val paint: Paint = Paint().apply {
+        strokeWidth = 1f
+        style = Paint.Style.STROKE
+        color = Color.rgb(0, 255, 0)
     }
+    private val bytesBus = BehaviorSubject.create<ByteArray>()
 
     fun update(bytes: ByteArray) {
         bytesBus.onNext(bytes)
@@ -37,27 +31,21 @@ class WavetablePreview @JvmOverloads constructor(
             return
         }
         val mBytes = bytesBus.blockingFirst()
-        if (mPoints.size != mBytes.size * 8) {
-            mPoints = FloatArray(mBytes.size * 8)
-        }
-        mRect[0, 0, width] = height
         val length = mBytes.size - 1
-        val height = mRect.height()
-
+        val path = Path()
 
         for (i in 0 until length) {
-            val index = i * 8
-            val m = Byte.MAX_VALUE
-            mPoints[index] = (mRect.width() * i / length).toFloat()
-            mPoints[index + 1] = height / 2 + ((mBytes[i]) * (height / 2) / m).toFloat()
-            mPoints[index + 2] = (mRect.width() * (i + 1) / length).toFloat()
-            mPoints[index + 3] = height / 2 - (mBytes[i] * (height / 2) / m).toFloat()
-
-            mPoints[4 + index] = (mRect.width() * i / length).toFloat()
-            mPoints[4 + index + 1] = height / 2 + ((mBytes[i]) * (height / 2) / m).toFloat()
-            mPoints[4 + index + 2] = (mRect.width() * (i + 1) / length).toFloat()
-            mPoints[4 + index + 3] = height / 2 - (mBytes[i] * (height / 2) / m).toFloat()
+            val relativeLoudness = max(((mBytes[i]) * (height / 2) / Byte.MAX_VALUE), 1)
+            val start = width * (i - 1) / length
+            val end = width * (i + 1) / length
+            val rect = RectF(
+                start.toFloat(),
+                (height / 2 - relativeLoudness).toFloat(),
+                end.toFloat() + 100,
+                (height / 2 + relativeLoudness).toFloat(),
+            )
+            path.addRect(rect, Path.Direction.CCW)
         }
-        canvas.drawLines(mPoints, mForePaint)
+        canvas.drawPath(path, paint)
     }
 }
