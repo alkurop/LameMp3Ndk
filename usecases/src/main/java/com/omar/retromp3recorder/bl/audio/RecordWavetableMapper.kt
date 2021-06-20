faf73a89b1577794587b8f713765f432f4efe5f9
Alexey Kuropiatnyk <alexey.kuropiantnyk@ah.nl>
2021-06-20 21:31:45 +0200

    Extract wavetable mapper

--- usecases/src/main/java/com/omar/retromp3recorder/bl/audio/RecordWavetableMapper.kt
+++ usecases/src/main/java/com/omar/retromp3recorder/bl/audio/RecordWavetableMapper.kt
@@ -1,55 +1,20 @@
 package com.omar.retromp3recorder.bl.audio
 
-import com.github.alkurop.ghostinshell.Shell
-import com.omar.retromp3recorder.dto.Wavetable
 import com.omar.retromp3recorder.iorecorder.Mp3VoiceRecorder
-import com.omar.retromp3recorder.storage.repo.CurrentFileRepo
-import com.omar.retromp3recorder.storage.repo.WavetableRepo
-import io.reactivex.rxjava3.core.Completable
 import io.reactivex.rxjava3.core.Observable
 import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 import kotlin.math.absoluteValue
 
 class RecordWavetableMapper @Inject constructor(
-    private val recorder: Mp3VoiceRecorder,
-    private val audioStateMapper: AudioStateMapper,
-    private val wavetableRepo: WavetableRepo,
-    private val currentFileRepo: CurrentFileRepo
+    private val recorder: Mp3VoiceRecorder
 ) {
-    fun execute(): Completable {
-        return observeRecorder(WAVETABLE_SIZE_MILLIS)
-            .takeUntil(
-                audioStateMapper.observe()
-                    .ofType(AudioState.Idle::class.java)
-            )
-            .collectInto(mutableListOf<Byte>(), { list, item -> list.add(item) })
-            .map { it.toByteArray() }
-            .flatMapCompletable {
-                Completable.fromAction {
-                    val ghost = Wavetable(
-                        it
-                    )
-                    val currentFilePath = currentFileRepo.observe().blockingFirst().value!!
-                    val pair = Pair(
-                        currentFilePath,
-                        ghost
-                    )
-                    wavetableRepo.onNext(
-                        Shell(
-                            pair
-                        )
-                    )
-                }
-            }
-    }
-
-    private fun observeRecorder(windowMillis: Long): Observable<Byte> {
+    fun observe(): Observable<Byte> {
         return recorder.observeRecorder()
             .map { array ->
                 array.toList().map { it.toInt().absoluteValue }.maxOrNull() ?: 0
             }
-            .buffer(windowMillis, TimeUnit.MILLISECONDS)
+            .buffer(WAVETABLE_SIZE_MILLIS, TimeUnit.MILLISECONDS)
             .map { it.maxOrNull() ?: 0 }
             .map { it.toByte() }
     }
