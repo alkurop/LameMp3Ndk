8431b04ef6193a90518ba60144db645c33c600c1
Alexey Kuropiatnyk <alexey.kuropiantnyk@ah.nl>
2021-06-23 20:58:53 +0200

    Remove some logs

--- app/src/main/java/com/omar/retromp3recorder/app/ui/joined_progress/JoinedProgressFragment.kt
+++ app/src/main/java/com/omar/retromp3recorder/app/ui/joined_progress/JoinedProgressFragment.kt
@@ -13,7 +13,6 @@ import com.omar.retromp3recorder.ui.wavetable.WavetablePreview
 import com.omar.retromp3recorder.ui.wavetable.WavetableSeekbarPreview
 import com.omar.retromp3recorder.utils.toDisplay
 import com.omar.retromp3recorder.utils.toSeekbarTime
-import timber.log.Timber
 
 class JoinedProgressFragment : Fragment(R.layout.fragment_joined_progress) {
     private val viewModel by viewModels<JoinedProgressViewModel>()
@@ -40,10 +39,10 @@ class JoinedProgressFragment : Fragment(R.layout.fragment_joined_progress) {
     }
 
     private fun renderView(joinedProgress: JoinedProgress) {
-        Timber.d("Joined progress, $joinedProgress")
         playerProgress.isGone = joinedProgress !is JoinedProgress.PlayerProgressShown
         recorderProgress.isGone = joinedProgress !is JoinedProgress.RecorderProgressShown
         recorderWavetable.isGone = joinedProgress !is JoinedProgress.RecorderProgressShown
+
         when (joinedProgress) {
             is JoinedProgress.RecorderProgressShown -> {
                 recorderProgress.text = joinedProgress.progress.toDisplay()
