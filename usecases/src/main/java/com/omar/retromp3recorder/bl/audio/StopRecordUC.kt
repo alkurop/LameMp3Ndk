9bfb3e0643e0aca92a029bd15081a6e879a81189
Alexey Kuropiantyk <alkurop@gmail.com>
2021-07-10 22:46:35 +0200

    Freeze killer (#61)
    * Remove extra logs and optimize saving id3 tags
    
    * Log audio status
    
    * Fix slow saving issue
    
    * Fix wavetable preview
    
    * Fix wavetable layout
    
    * Increment version

--- usecases/src/main/java/com/omar/retromp3recorder/bl/audio/StopRecordUC.kt
+++ usecases/src/main/java/com/omar/retromp3recorder/bl/audio/StopRecordUC.kt
@@ -1,6 +1,5 @@
 package com.omar.retromp3recorder.bl.audio
 
-import com.omar.retromp3recorder.bl.files.FindFilesUC
 import com.omar.retromp3recorder.iorecorder.Mp3VoiceRecorder
 import com.omar.retromp3recorder.storage.repo.CurrentFileRepo
 import com.omar.retromp3recorder.utils.Mp3TagsEditor
@@ -12,10 +11,9 @@ import javax.inject.Inject
 
 class StopRecordUC @Inject constructor(
     private val currentFileRepo: CurrentFileRepo,
-    private val findFilesUC: FindFilesUC,
     private val mp3TagsEditor: Mp3TagsEditor,
     private val recordingTagsDefaultProvider: RecordingTagsDefaultProvider,
-    private val saveWavetableUC: SaveWavetableUC,
+    private val saveRecordingWithWavetableUC: SaveRecordingWithWavetableUC,
     private val scheduler: Scheduler,
     private val voiceRecorder: Mp3VoiceRecorder
 ) {
@@ -23,26 +21,25 @@ class StopRecordUC @Inject constructor(
         .fromAction {
             voiceRecorder.stopRecord()
         }
-        .andThen(findFilesUC.execute())
-        .andThen(saveWavetableUC.execute())
-        //silly way to update current file preview and show wavetable of recently
-        //recorded file
+        .andThen(saveRecordingWithWavetableUC.execute())
         .andThen(currentFileRepo.observe().takeOne().flatMapCompletable { currentFileWrapper ->
-            Completable
-                .fromAction {
-                    val filepath = currentFileWrapper.value!!
-                    mp3TagsEditor.setTags(
-                        filepath,
-                        recordingTagsDefaultProvider.provideDefaults().copy(
-                            title = mp3TagsEditor.getFilenameFromPath(filepath)
+            Completable.merge(
+                listOf(
+                    Completable.fromAction {
+                        val filepath = currentFileWrapper.value!!
+                        mp3TagsEditor.setTags(
+                            filepath,
+                            recordingTagsDefaultProvider.provideDefaults().copy(
+                                title = mp3TagsEditor.getFilenameFromPath(filepath)
+                            )
                         )
-                    )
-                }
-                .andThen(
+                    },
+                    //silly way to update current file preview and show wavetable of recently
+                    //recorded file
                     Completable.fromAction {
                         currentFileRepo.onNext(currentFileWrapper)
                     }
-                )
+                ))
         })
         .subscribeOn(scheduler)
 }
