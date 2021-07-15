c0e5e839a1f2cd67e067737b69e27949e85664da
Alexey Kuropiatnyk <alkurop@gmail.com>
2021-07-15 11:47:56 +0200

    Simplify save file usecase

--- usecases/src/main/java/com/omar/retromp3recorder/bl/audio/SaveRecordingWithWavetableUC.kt
+++ usecases/src/main/java/com/omar/retromp3recorder/bl/audio/SaveRecordingWithWavetableUC.kt
@@ -5,6 +5,8 @@ import com.omar.retromp3recorder.storage.db.toDatabaseEntity
 import com.omar.retromp3recorder.storage.repo.FileListRepo
 import com.omar.retromp3recorder.storage.repo.WavetableRepo
 import com.omar.retromp3recorder.utils.FileLister
+import com.omar.retromp3recorder.utils.Mp3TagsEditor
+import com.omar.retromp3recorder.utils.RecordingTagsDefaultProvider
 import com.omar.retromp3recorder.utils.takeOne
 import io.reactivex.rxjava3.core.Completable
 import javax.inject.Inject
@@ -13,21 +15,38 @@ class SaveRecordingWithWavetableUC @Inject constructor(
     private val appDatabase: AppDatabase,
     private val wavetableRepo: WavetableRepo,
     private val fileListRepo: FileListRepo,
-    private val fileLister: FileLister
+    private val fileLister: FileLister,
+    private val saveMp3TagsUC: SaveMp3TagsUC
 ) {
     fun execute(): Completable =
         wavetableRepo.observe()
             .takeOne()
             .flatMapCompletable { shell ->
                 val (path, wave) = shell
-                Completable.fromAction {
-                    val fileEntityDao = appDatabase.fileEntityDao()
-                    val newItem = fileLister.discoverFile(path).copy(wavetable = wave)
+                saveMp3TagsUC.execute(path).andThen(
+                    Completable
+                        .fromAction {
+                            val fileEntityDao = appDatabase.fileEntityDao()
+                            val newItem = fileLister.discoverFile(path).copy(wavetable = wave)
 
-                    fileEntityDao.insert(listOf(newItem.toDatabaseEntity()))
-                    val fileList = fileListRepo.observe().blockingFirst()
-                    //update filelist with the wavetable for first file
-                    fileListRepo.onNext(fileList + listOf(newItem))
-                }
+                            fileEntityDao.insert(listOf(newItem.toDatabaseEntity()))
+                            val fileList = fileListRepo.observe().blockingFirst()
+                            //update filelist with the wavetable for first file
+                            fileListRepo.onNext(fileList + listOf(newItem))
+                        })
             }
+}
+
+class SaveMp3TagsUC @Inject constructor(
+    private val mp3TagsEditor: Mp3TagsEditor,
+    private val recordingTagsDefaultProvider: RecordingTagsDefaultProvider
+) {
+    fun execute(filepath: String): Completable = Completable.fromAction {
+        mp3TagsEditor.setTags(
+            filepath,
+            recordingTagsDefaultProvider.provideDefaults().copy(
+                title = mp3TagsEditor.getFilenameFromPath(filepath)
+            )
+        )
+    }
 }
--- usecases/src/main/java/com/omar/retromp3recorder/bl/audio/StopRecordUC.kt
+++ usecases/src/main/java/com/omar/retromp3recorder/bl/audio/StopRecordUC.kt
@@ -2,8 +2,6 @@ package com.omar.retromp3recorder.bl.audio
 
 import com.omar.retromp3recorder.iorecorder.Mp3VoiceRecorder
 import com.omar.retromp3recorder.storage.repo.CurrentFileRepo
-import com.omar.retromp3recorder.utils.Mp3TagsEditor
-import com.omar.retromp3recorder.utils.RecordingTagsDefaultProvider
 import com.omar.retromp3recorder.utils.takeOne
 import io.reactivex.rxjava3.core.Completable
 import io.reactivex.rxjava3.core.Scheduler
@@ -11,8 +9,6 @@ import javax.inject.Inject
 
 class StopRecordUC @Inject constructor(
     private val currentFileRepo: CurrentFileRepo,
-    private val mp3TagsEditor: Mp3TagsEditor,
-    private val recordingTagsDefaultProvider: RecordingTagsDefaultProvider,
     private val saveRecordingWithWavetableUC: SaveRecordingWithWavetableUC,
     private val scheduler: Scheduler,
     private val voiceRecorder: Mp3VoiceRecorder
@@ -23,23 +19,11 @@ class StopRecordUC @Inject constructor(
         }
         .andThen(saveRecordingWithWavetableUC.execute())
         .andThen(currentFileRepo.observe().takeOne().flatMapCompletable { currentFileWrapper ->
-            Completable.merge(
-                listOf(
-                    Completable.fromAction {
-                        val filepath = currentFileWrapper.value!!
-                        mp3TagsEditor.setTags(
-                            filepath,
-                            recordingTagsDefaultProvider.provideDefaults().copy(
-                                title = mp3TagsEditor.getFilenameFromPath(filepath)
-                            )
-                        )
-                    },
-                    //silly way to update current file preview and show wavetable of recently
-                    //recorded file
-                    Completable.fromAction {
-                        currentFileRepo.onNext(currentFileWrapper)
-                    }
-                ))
+            //silly way to update current file preview and show wavetable of recently
+            //recorded file
+            Completable.fromAction {
+                currentFileRepo.onNext(currentFileWrapper)
+            }
         })
         .subscribeOn(scheduler)
 }
