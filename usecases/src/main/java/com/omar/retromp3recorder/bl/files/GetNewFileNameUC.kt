97734b4dc207039b7126ef5fd02100b5efd0be79
Alexey Kuropiatnyk <alkurop@gmail.com>
2021-07-15 12:37:21 +0200

    Fix file name

--- usecases/src/main/java/com/omar/retromp3recorder/bl/files/GetNewFileNameUC.kt
+++ usecases/src/main/java/com/omar/retromp3recorder/bl/files/GetNewFileNameUC.kt
@@ -4,6 +4,7 @@ import android.content.SharedPreferences
 import com.omar.retromp3recorder.storage.SharedPrefsKeys
 import com.omar.retromp3recorder.utils.FilePathGenerator
 import io.reactivex.rxjava3.core.Single
+import java.util.*
 import javax.inject.Inject
 
 /**
@@ -14,13 +15,22 @@ class GetNewFileNameUC @Inject constructor(
     private val sharedPreferences: SharedPreferences
 ) {
     fun execute(): Single<String> {
+        val calendar = Calendar.getInstance()
+        calendar.timeInMillis = System.currentTimeMillis()
+        val hours = calendar.get(Calendar.HOUR_OF_DAY)
+        val minutes = calendar.get(Calendar.MINUTE)
+        val day = calendar.get(Calendar.DAY_OF_MONTH)
+        val month = calendar.get(Calendar.MONTH)
+        val year = calendar.get(Calendar.YEAR)
+        val dateFormat =
+            "${day}-${month}-${year}_$hours-$minutes"
+
         return Single.fromCallable {
-            "${filePathGenerator.generateFilePath()}/${
-                sharedPreferences.getInt(
-                    SharedPrefsKeys.FILE_NAME,
-                    1
-                )
-            }.mp3"
+            val int = sharedPreferences.getInt(
+                SharedPrefsKeys.FILE_NAME,
+                1
+            )
+            "${filePathGenerator.generateFilePath()}/${dateFormat}_($int).mp3"
         }
     }
 }
