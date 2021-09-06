1a75497015e081f6143253e152426d1bab385caa
Alex Kurop <alkurop@gmail.com>
2021-09-06 11:58:10 +0200

    [Menu] Move settings to menu item, remove the title banenr

--- app/src/main/java/com/omar/retromp3recorder/app/ui/main/MainActivity.kt
+++ app/src/main/java/com/omar/retromp3recorder/app/ui/main/MainActivity.kt
@@ -3,9 +3,12 @@ package com.omar.retromp3recorder.app.ui.main
 import android.Manifest
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import com.github.alkurop.jpermissionmanager.PermissionOptionalDetails
 import com.github.alkurop.jpermissionmanager.PermissionRequiredDetails
 import com.github.alkurop.jpermissionmanager.PermissionsManager
@@ -18,18 +21,30 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
     private val permissionsManager: PermissionsManager by lazy { PermissionsManager(this) }
     private val permissionsMap: Map<String, PermissionOptionalDetails> by lazy { createPermissionsMap() }
     private val viewModel by viewModels<MainViewModel>()
-    private val titleContainer by lazy { findViewById<View>(R.id.titleContainer) }
+    private val toolbar by lazy { findViewById<Toolbar>(R.id.toolbar) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         viewModel.state.observe(this, ::renderView)
-        titleContainer.setOnClickListener {
-            startActivity(Intent(this, SettingsActivity::class.java))
-        }
+        setSupportActionBar(toolbar)
     }
 
     private fun renderView(state: MainView.State) {
         renderPermissions(state.requestForPermissions.ghost)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.main_activity, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.settings) {
+            startActivity(Intent(this, SettingsActivity::class.java))
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     private fun createPermissionsMap(): Map<String, PermissionOptionalDetails> =
